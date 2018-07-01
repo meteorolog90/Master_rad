2a2bf0e77dee3ebf55f7a0b8854ca93954e1ec79
Martin <38101274+meteorolog90@users.noreply.github.com>
2018-07-01 14:17:44 +0200

    Add files via upload

--- test.py
+++ test.py
@@ -1,14 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x1,x2 = np.mgrid[17:27:0.1,50:44:0.1]
+xx,yy = np.mgrid[17:27:0.1,44:50:0.1]
 
 
+plt.figure(num=None, figsize=(10, 8), dpi=50, facecolor='w', edgecolor='k')
+
+#plt.figure(num=None, figsize=(20, 18), dpi=200, facecolor='w', edgecolor='k')
 plt.title('mgrid (dense meshgrid)')
 plt.grid()
-plt.xticks(xx[99])
-plt.yticks(yy[:, 59])
+plt.xticks(xx[0]) #izmedju 17-28 imamo 100 tacaka
+plt.yticks(yy[:,0]) #izmedju 44-50 imam 60 tacaka
 plt.scatter(xx, yy, color="red", marker="x")
-plt.figure()
 
+#print(xx[0])
 
